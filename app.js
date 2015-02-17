05c73fa01968783b96c260c4dac7f49963b50f84
Andrew Johnston <apjohnsto@gmail.com>
2015-02-17 23:32:56 +0000

    update app

--- app.js
+++ app.js
@@ -1,27 +1,14 @@
-var port = process.env.PORT || 3000,
-    http = require("http"),
-    fs = require("fs"),
-    html = fs.readFileSync("index.html");
-
-var server = http.createServer(function (request, response) {
-    if (request.method == 'POST') {
-        request.on('data', function(chunk) {
-            fs.appendFile("/tmp/sample-app.log", chunk.toString() + "\n", function (err) {});
-        });
-        
-        request.on('end', function() {
-            response.writeHead(200, "OK", {'Content-Type': 'text/html'});
-            response.end();
-        });
-    } else {
-        response.writeHead(200);
-        response.write(html);
-        response.end();
-    }
+var port = process.env.PORT || 3000;
+var sse = require('connect-sse')();
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname + '/public'));
+app.get('/events', sse, function (req, res) {
+  setInterval(function() {
+    res.json({
+      message: "ping",
+      time: Date.now()
+    })
+  }, 1000);
 });
-
-// Listen on port 3000, IP defaults to 127.0.0.1
-server.listen(port);
-
-// Put a friendly message on the terminal
-console.log("Server running at http://127.0.0.1:" + port + "/");
+app.listen(port);
